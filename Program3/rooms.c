f0126a9d163b22ecd784e33bce6bdd7905dd2397
AlexanderMinser <adminser@ncsu.edu>
2017-11-28 16:12:22 -0500

    Started on rooms.c

--- Program3/rooms.c
+++ Program3/rooms.c
@@ -4,18 +4,37 @@
 ** rooms.c
 */
 
-
+Schedule* rooms;
+int numberRooms;
 
 void setupRooms(int nrooms, Time open, Time close){
-
+    rooms = (Schedule*) malloc(sizeof(Schedule)*nrooms);
+    numberRooms = nrooms;
+    int i;
+    Schedule s;
+    for(i=0; i<nrooms; i++) {
+        s = createSchedule(open, close);
+        rooms[i] = s;
+    }
 }
 
 int numRooms(){
-
+    return numberRooms;
 }
 
 int makeReservation(const char* name, Time start, Time end){
-
+    int i;
+    int flag;
+    for(i=0; i<numberRooms; i++) {
+        if(!isBusy(rooms[i], start, end)){
+            flag = reserve(rooms[i], name, start, end);
+            if (!flag){
+                printf("something has gone wrong\n");
+            }
+            return i+1; /* must be 1 greater than i since room nums start at 1*/
+        }
+    }
+    return 0;
 }
 
 int cancelReservation(int room, const char* name, Time start){
@@ -31,5 +50,5 @@ void printReservations(const struct rNode* list, FILE* stream){
 }
 
 void printRoomSchedules(FILE* stream){
-    
+
 }
